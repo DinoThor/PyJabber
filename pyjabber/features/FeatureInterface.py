5d406905c95f58939207a66672da66154ed1d3bc
manel <manelbng@gmail.com>
2024-06-27 12:08:30 +0200

    skip coverage

--- pyjabber/features/FeatureInterface.py
+++ pyjabber/features/FeatureInterface.py
@@ -3,8 +3,7 @@ import xml.etree.ElementTree as ET
 from abc import ABCMeta
 from abc import abstractmethod
 
-
-class FeatureInterface(metaclass = ABCMeta):
+class FeatureInterface(metaclass=ABCMeta):
     @abstractmethod
-    def feed(element: ET.Element, extra: dict[str, any] = None):
-        pass
+    def feed(element: ET.Element, extra: dict[str, any] = None):  # pragma: no cover
+        pass  # pragma: no cover
