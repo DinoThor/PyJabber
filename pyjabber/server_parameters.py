3aacd92933b7572a2d43708594ab2190f48239ce
DinoThor <aaron.raya.lopez@gmail.com>
2025-04-08 10:31:41 +0200

    Added pickle dump/load to Paramters

--- pyjabber/server_parameters.py
+++ pyjabber/server_parameters.py
@@ -1,7 +1,31 @@
 import json
 import os
+import pickle
 import socket
 from dataclasses import dataclass, replace
+
+
+def load(file_path: str):
+    with open(file_path, "rb") as data:
+        return pickle.load(data)
+
+
+def from_json(file_path: str):
+    with open(file_path, "r") as file:
+        loaded = json.load(file)
+        return Parameters(
+            host=loaded.get('host') or "localhost",
+            client_port=loaded.get('client_port') or 5222,
+            server_port=loaded.get('server_port') or 5269,
+            server_out_port=loaded.get('server_out_port') or 5269,
+            family=loaded.get('family') or socket.AF_INET,
+            connection_timeout=loaded.get('connection_timeout') or 60,
+            database_path=loaded.get('database_path') or os.path.join(os.getcwd(), "pyjabber.db"),
+            database_purge=loaded.get('database_purge') or False,
+            database_in_memory=loaded.get('database_in_memory') or False,
+            cert_path=loaded.get('cert_path') or None,
+            message_persistence=loaded.get('message_persistence') is True
+        )
 
 
 @dataclass(frozen=True)
@@ -18,7 +42,11 @@ class Parameters:
     cert_path: str = None
     message_persistence: bool = True
 
-    def from_json(self, file_path: str):
+    def dump(self, file_path: str):
+        with open(file_path, "wb") as data:
+            pickle.dump(self, data)
+
+    def update_from_json(self, file_path: str):
         with open(file_path, "r") as file:
             loaded = json.load(file)
             return replace(
--- test/test_server_parameters.py
+++ test/test_server_parameters.py
@@ -3,7 +3,7 @@ import os
 import socket
 from unittest.mock import mock_open, patch
 
-from pyjabber.server_parameters import Parameters
+from pyjabber.server_parameters import Parameters, from_json, load
 
 
 def test_default_parameters():
@@ -20,7 +20,38 @@ def test_default_parameters():
     assert param.cert_path is None
     assert param.message_persistence is True
 
-def test_load_json():
+def test_from_json():
+    datamock = {
+        'host': 'publichost',
+        'client_port': 5252,
+        'server_port': 2525,
+        'server_out_port': 6952,
+        'family': socket.AF_INET6.value,
+        'connection_timeout': 59,
+        'database_path': '/some/fake/path',
+        'database_purge': True,
+        'database_in_memory': True,
+        'cert_path': '/another/fake/path',
+        'message_persistence': False
+    }
+    m = mock_open(read_data=json.dumps(datamock))
+
+    with patch("pyjabber.server_parameters.open", m):
+        param = from_json("fake.json")
+
+    assert param.host == datamock["host"]
+    assert param.client_port == datamock["client_port"]
+    assert param.server_port == datamock["server_port"]
+    assert param.server_out_port == datamock["server_out_port"]
+    assert param.family == datamock["family"]
+    assert param.connection_timeout == datamock["connection_timeout"]
+    assert param.database_path == datamock["database_path"]
+    assert param.database_purge is datamock["database_purge"]
+    assert param.database_in_memory is datamock["database_in_memory"]
+    assert param.cert_path == datamock["cert_path"]
+    assert param.message_persistence is datamock["message_persistence"]
+
+def test_update_from_json():
     datamock = {
         'host': 'publichost',
         'client_port':  5252,
@@ -37,7 +68,7 @@ def test_load_json():
     m = mock_open(read_data=json.dumps(datamock))
 
     with patch("pyjabber.server_parameters.open", m):
-        param = Parameters().from_json("fake.json")
+        param = Parameters().update_from_json("fake.json")
 
     assert param.host == datamock["host"]
     assert param.client_port == datamock["client_port"]
@@ -50,3 +81,28 @@ def test_load_json():
     assert param.database_in_memory is datamock["database_in_memory"]
     assert param.cert_path == datamock["cert_path"]
     assert param.message_persistence is datamock["message_persistence"]
+
+
+def test_pickle():
+    filepath = './params.pkl'
+    param = Parameters()
+    param.dump(filepath)
+
+    assert os.path.exists(filepath)
+
+    parsed_param = load(filepath)
+
+    assert parsed_param.host == "localhost"
+    assert parsed_param.client_port == 5222
+    assert parsed_param.server_port == 5269
+    assert parsed_param.server_out_port == 5269
+    assert parsed_param.family == socket.AF_INET
+    assert parsed_param.connection_timeout == 60
+    assert parsed_param.database_path == os.path.join(os.getcwd(), "pyjabber.db")
+    assert parsed_param.database_purge is False
+    assert parsed_param.database_in_memory is False
+    assert parsed_param.cert_path is None
+    assert parsed_param.message_persistence is True
+
+    if os.path.exists(filepath):
+        os.remove(filepath)
